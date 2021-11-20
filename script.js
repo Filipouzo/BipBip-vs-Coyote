5c751b94759589ab713ee88f86665081971722b7
Philippe ARNAL <philiparnal@gmail.com>
2021-11-20 15:47:31 +0100

    HTML ready

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+
+// const newGame = document.querySelector('#newGame');
+const roll = document.querySelector('#roll');
+const secure = document.querySelector('#secure');
+
+
+function startNewGame() {
+    console.log('commencer nouveau jeu');
+  }
